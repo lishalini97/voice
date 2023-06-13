e47ddb9fef456bb73e81a55edb37de12c85c4dca
Lishalini Manogaran <82559075+lishalini97@users.noreply.github.com>
2023-06-13 21:48:52 +0800

    Update speechrecognition.py

--- speechrecognition.py
+++ speechrecognition.py
@@ -3,32 +3,21 @@ import speech_recognition as sr
 # Initialize recognizer class (for recognizing the speech)
 r = sr.Recognizer()
 
-def listen_and_process(retry_count=0):
+def listen_and_process():
     # Use the microphone as source for input.
     with sr.Microphone() as source:
-        print("Please say something...")
-        
         # Record the audio
         audio = r.listen(source)
         
         try:
             # Using google speech recognition
-            print("Processing, please wait...")
             text = r.recognize_google(audio)
             if "I am sad" in text:
-                print("User said 'I am sad'.")
+                pass  # User said 'I am sad', do something here
             else:
-                if retry_count < 10:
-                    print("Sorry, I didn't get that. Can you repeat please?")
-                    listen_and_process(retry_count+1)  # repeat the process
-                else:
-                    print("Sorry, I was unable to recognize your speech. Please try again later.")
+                listen_and_process()  # repeat the process
         except:
-            if retry_count < 10:
-                print("Sorry, I did not get that. Can you repeat please?")
-                listen_and_process(retry_count+1)  # repeat the process
-            else:
-                print("Sorry, I was unable to recognize your speech. Please try again later.")
+            listen_and_process()  # repeat the process
 
 # Call the function to start
 listen_and_process()
